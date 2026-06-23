44088a763fb25bc3a8823452c4562cef5d4ddb2d
Nithinaug <nithinaug@gmail.com>
2026-06-23 16:04:12 +0800

    removed unused functions

--- api/presidio.py
+++ api/presidio.py
@@ -1,3 +1,4 @@
+import re
 from functools import lru_cache
 from typing import List
 
@@ -9,6 +10,34 @@ from presidio_image_redactor import ImageRedactorEngine, ImageAnalyzerEngine
 from .fixes import build_custom_recognizers, reclassify, dedupe_results, merge_adjacent
 
 MIN_SCORE = 0.5
+
+_DATE_PATTERN = re.compile(
+    r"\b\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4}\b"
+    r"|\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*\.?\s+\d{1,2},?\s*\d{2,4}\b"
+    r"|\b\d{1,2}\s+(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*\.?\s+\d{2,4}\b"
+    r"|\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*\.?\s+\d{4}\b"
+    r"|\b(?:19|20)\d{2}\b",
+    re.IGNORECASE,
+)
+
+_ADDRESS_WORDS = re.compile(
+    r"\b(?:St|Rd|Ave|Blvd|Dr|Ln|Ct|Pl|Way|Hwy|Pkwy|Cir|Ter|Loop|Trail|"
+    r"Street|Road|Avenue|Boulevard|Drive|Lane|Court|Place|Highway|Parkway|Circle|"
+    r"D\.?C\.?|VA|MD|CA|NY|TX|FL|NJ|PA|IL|OH|GA|NC|MI|WA|AZ|MA|TN|IN|MO|WI|MN|CO|AL|SC|"
+    r"Arlington|Washington|Manhattan|Brooklyn|Boston|Chicago|Houston|Phoenix|Philadelphia|"
+    r"San\s+(?:Francisco|Diego|Antonio|Jose)|Los\s+Angeles|New\s+York|"
+    r"\d{5}(?:-\d{4})?)\b",
+    re.IGNORECASE,
+)
+
+_LOCATION_FRAGMENT = re.compile(r"^[\s,]*(?:[A-Z]{2}|D\.?C\.?|(?:,?\s*[A-Z][a-z]+)+,?\s*[A-Z]{2})[\s,.]*$")
+
+_BANK_CONTEXT = re.compile(
+    r"\b(?:account|acct|bank|IBAN|SWIFT|sort\s*code|routing|DBS|OCBC|UOB|POSB|Citibank|HSBC|Standard\s*Chartered)\b",
+    re.IGNORECASE,
+)
+
+_PHONE_CONTEXT = re.compile(r"\b(?:phone|mobile|contact|call|tel|hp|fax)\b", re.IGNORECASE)
 
 
 @lru_cache(maxsize=1)
