459e2dfa2789ae75f592479c943ef70a285c8a87
Nithinaug <nithinaug@gmail.com>
2026-06-24 17:02:36 +0800

    updated CORS

--- api/main.py
+++ api/main.py
@@ -17,7 +17,7 @@ MAX_UPLOAD_BYTES = 25 * 1024 * 1024
 app = FastAPI(title="Redactor API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:5173").split(","),
+    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:5173,http://localhost:4173").split(","),
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["Content-Type"],
 )
